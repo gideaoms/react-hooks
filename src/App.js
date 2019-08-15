aa7adf6b8fe8eadb379e910a41645229fc5feec4
Gideão Silva <gideaoms@gmail.com>
2019-08-15 11:18:34 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+const LOCAL_STORAGE_NAME = '@tecnologies';
 
 function App() {
+  const [tecnologies, setTecnologies] = useState([]);
+  const [newTecnology, setNewTecnology] = useState('');
+
+  useEffect(() => {
+    const storedTecnologies = localStorage.getItem(LOCAL_STORAGE_NAME);
+    if (storedTecnologies) {
+      setTecnologies(JSON.parse(storedTecnologies));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(tecnologies));
+  }, [tecnologies]);
+
+  const tecnologiesSize = useMemo(() => {
+    return tecnologies.length;
+  }, [tecnologies]);
+
+  const handleAdd = useCallback(() => {
+    setTecnologies([...tecnologies, newTecnology]);
+    setNewTecnology('');
+  }, [newTecnology, tecnologies]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {tecnologies.map(tecnology => (
+          <li key={tecnology}>{tecnology}</li>
+        ))}
+      </ul>
+
+      <p>Você tem {tecnologiesSize} tecnologias</p>
+
+      <input
+        type="text"
+        onChange={event => setNewTecnology(event.target.value)}
+        value={newTecnology}
+      />
+
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
   );
 }
 
